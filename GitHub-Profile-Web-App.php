5ee19f10cb317cf0e31420ee5d6ad44db5622ad6
James Phillips <JamesPhillipsUK@users.noreply.github.com>
2018-07-02 18:52:31 +0100

    Update GitHub-Profile-Web-App.php
    Here's the Vanilla (plain) variant of the latest release of my GitHub Profile Web App.
    This is predominantly unstyled by default, it will take up the default styles from your stylesheet, or you can alter it to add whatever style you like.

--- GitHub-Profile-Web-App.php
+++ GitHub-Profile-Web-App.php
@@ -1,9 +1,34 @@
 <?php
-//Put this block in the header of the page to decrease load time.
+/**
+ *
+ * GitHub Profile Web App, Version 2.0.0, Vanilla variant
+ *
+ * GitHub Profile Web App is designed to give a rundown of your GitHub Profile on your PHP Website
+ * Copyright (C) 2017, 2018  James Phillips <james@jamesphillipsuk.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  *
+  **/
+
+	//Put this block in the header of the page to decrease load time.
+    define("PERSONALACCESSTOKEN","IT-GOES-HERE"); //Add your Personal Access Token here
+    define("GITHUBUSERNAME","IT-GOES-HERE"); //Add your GitHub username here
+
 	function CallGitHub() //this function pulls the user's GitHub Profile data
 	{
 		$GitHubURL = "https://api.github.com/user"; //pull your user data from GitHub
-		$GitHubHeaders = array('User-Agent: jamesphillipsuk-GitHubPHPApp','Authorization: token INSERT YOUR TOKEN HERE',); //insert your personal access token
+		$GitHubHeaders = array('User-Agent: jamesphillipsuk-GitHubPHPApp','Authorization: token ' . PERSONALACCESSTOKEN . '',); //insert your personal access token
 		$GitHubcURL = curl_init(); //initialize cURL
 		if(curl_error($GitHubcURL))
 		{
@@ -13,7 +38,6 @@
 		curl_setopt($GitHubcURL, CURLOPT_HTTPHEADER, $GitHubHeaders);  //send our authorization headers
 		curl_setopt($GitHubcURL, CURLOPT_RETURNTRANSFER, 1); //don't print it to the screen yet
 		curl_setopt($GitHubcURL, CURLOPT_CUSTOMREQUEST, "GET"); //use GET to grab the data
-
 		$GitHubJSON = json_decode(curl_exec($GitHubcURL)); //decode the returned JSON data
 		curl_close($GitHubcURL); //close cURL
 		return $GitHubJSON; //return the decoded JSON
@@ -30,8 +54,8 @@
 	
 	function CallGitHubRepos() //this function pulls the user's GitHub repository data
 	{
-		$GitHubURL = "https://api.github.com/users/YOUR GITHUB USERNAME/repos"; //the GitHub URL for the user's repos
-		$GitHubHeaders = array('User-Agent: jamesphillipsuk-GitHubPHPApp','Authorization: token INSERT YOUR TOKEN HERE',); //insert your personal access token
+		$GitHubURL = "https://api.github.com/users/" . GITHUBUSERNAME . "/repos"; //the GitHub URL for the user's repos
+		$GitHubHeaders = array('User-Agent: jamesphillipsuk-GitHubPHPApp','Authorization: token ' . PERSONALACCESSTOKEN . '',); //insert your personal access token
 		$GitHubcURL = curl_init(); //initialize cURL
 		if(curl_error($GitHubcURL))
 		{
@@ -41,7 +65,6 @@
 		curl_setopt($GitHubcURL, CURLOPT_HTTPHEADER, $GitHubHeaders); 	//These headers are as before
 		curl_setopt($GitHubcURL, CURLOPT_RETURNTRANSFER, 1);		//
 		curl_setopt($GitHubcURL, CURLOPT_CUSTOMREQUEST, "GET");		//
-
 		$GitHubJSON = json_decode(curl_exec($GitHubcURL)); //decode the response
 		curl_close($GitHubcURL); // close cURL
 		return $GitHubJSON; //return the value of the decoded JSON data
@@ -49,52 +72,49 @@
 ?>
 
 <!-- INSERT THE SECTION BELOW WHERE YOU WANT YOUR PROFILE TO APPEAR -->
-<div id="GitHubAPI" style="margin: 0 auto 0 auto;">
-
+<div id="GitHubAPI" style="margin: 0 auto;">
 <?php
 	echo "
-	<div style='width:94%;margin: 0 auto 0 auto;padding:0 1% 0 1%;border: 0.1rem #CECECE solid;border-radius:0.5rem;'>
-		<h4><a style='color: inherit;text-decoration:none;' href='" . $GitHubHTMLURL . "'>Follow " . $GitHubName . " on GitHub</a></h4>
-		<hr style='margin:0 0 0 0;' />
-		<img src='" . $GitHubAvatarURL . "' alt='GitHub Avatar' style='width:25%;height:auto;display:inline-block;margin:0 auto 0 auto;' />
-		<div style='display:inline-block; max-width:75%;'>
-			<strong style='font-size:1rem;'>" . $GitHubName . "</strong>
-			<br />
-			<em style='font-size:0.8rem;'>&nbsp;" . $GitHubLogin . "</em>
-			<p style='font-size:0.6rem;'>" . $GitHubBio . "</p>
-		</div>
-		<table style='border: 0.1rem #CECECE solid;font-size:0.8rem; margin:0 auto 0 auto;'>
-			<thead>
-				<tr>
-					<th>Repos</th>
-					<th>Followers</th>
-					<th>Following</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>" . $GitHubPubRepos . "</td>
-					<td>" . $GitHubFollowers . "</td>
-					<td>" . $GitHubFollowing . "</td>
-				</tr>
-			</tbody>
-		</table>
-		<br />"; //Echo out the values we saved in the CallGitHub() function as fromatted HTML and CSS.
+	<section>
+		<h3><a href='" . $GitHubHTMLURL . "'>Follow " . $GitHubName . " on GitHub</a></h3>
+		<br />
+		<div>
+			<img src='" . $GitHubAvatarURL . "' alt='GitHub Avatar' style='width:24%;height:auto;display:inline-block;margin:0 auto;' />
+			<div style='display:inline-block; max-width:74%;'>
+				<strong>" . $GitHubName . "</strong>
+				<br />
+				<em>&nbsp;" . $GitHubLogin . "</em>
+				<p>" . $GitHubBio . "</p>
+			</div>
+			<table style='margin:0 auto;'>
+				<thead>
+					<tr>
+						<th>Repos</th>
+						<th>Followers</th>
+                        <th>Following</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>" . $GitHubPubRepos . "</td>
+						<td>" . $GitHubFollowers . "</td>
+						<td>" . $GitHubFollowing . "</td>
+					</tr>
+				</tbody>
+			</table>
+			<br />"; //Echo out the values we saved in the CallGitHub() function as fromatted HTML and CSS.
 	foreach($Repos = CallGitHubRepos() as $Data)
 	{
 		echo "
-		<div style='width:90%;height:auto;display:block;margin:0 auto 0.2rem auto;background-color:#ECECEC;'>
-			<h5 style='font-size:0.8rem;'><a style='color: inherit;text-decoration:none;' href='" . $Data->html_url . "'>" . $Data->name . "</a></h5>
-			<p style='font-size:0.6rem;'>" . $Data->description . "</p>
-			<p style='font-size:0.8rem;'>Primary Language: " . $Data->language . "</p>
-		</div>";
+			<div>
+				<strong><a href='" . $Data->html_url . "'>" . $Data->name . "</a></strong>
+				<p>" . $Data->description . "</p>
+				<p>Primary Language: " . $Data->language . "</p>
+			</div>";
 	} //list each of the user's repos individually.  This is optional as calling the function repeatedly adds to page load time drastically.
-  
-	echo "</div>"; //close off the UI
-	
-?>
-
-</div>
-<?php
+	echo "
+		</div>
+	</section>
+</div>"; //close off the UI
 	//That's all!
 ?>
